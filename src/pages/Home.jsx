aa4a0d94e30f4a438657ff19ae8d91bd4c5d92cf
Khan <loprommy55@gmail.com>
2025-08-06 10:56:45 +0700

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
         <>
             <section className='h-screen bg-cover bg-center flex items-center justify-between text-white' style={{ backgroundImage: 'url(https://images.pexels.com/photos/7983589/pexels-photo-7983589.jpeg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                 <div className='container mx-auto p-4'>
-                    <h2 className='text-5xl font-bold mb-4 text-blue-950'>NVC Training Center</h2>
-                    <p className='text-black mb-6 text-lg font-semibold'>Here We go again?</p>
+                    <h2 className='text-5xl font-bold mb-4 text-blue-950'>Hello World</h2>
+                    <p className='text-black mb-6 text-lg font-semibold'>Welcome to Server</p>
                 </div>
             </section>
         </>
