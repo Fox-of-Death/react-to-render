12b24bd5790b95cbd8fcbef42d95ddceb50bb394
Khan <loprommy55@gmail.com>
2025-08-06 11:02:41 +0700

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
         <>
             <footer className='bg-blue-800 text-white text-center'>
                 <div className='container mx-auto py-6 flex justify-between items-center'>
-                    <p className='text-md'>© 2023 NVC Training Center. All rights reserved.</p>
-                    <p className='text-md'>Made with ❤️ using React By Chanon Patiphimpakhom</p>
+                    <p className='text-md'>© 2023 Hello World. All rights reserved.</p>
+                    <p className='text-md'>Made with using React By Weerayut</p>
                 </div>
             </footer>
         </>
