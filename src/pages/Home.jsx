019ede49f354266572271449bed0e0090556691d
Khan <loprommy55@gmail.com>
2025-08-06 11:14:40 +0700

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import bgImage from '../img/2025_04_17_08_27_04_3182364.png';
 
 const Home = () => {
     return (
         <>
-            <section className='h-screen bg-cover bg-center flex items-center justify-between text-white' style={{ backgroundImage: 'url(/img/2025_04_17_08_27_04_3182364.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <section className='h-screen bg-cover bg-center flex items-center justify-between text-white' style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                 <div className='container mx-auto p-4'>
                     <h2 className='text-5xl font-bold mb-4 text-blue-950'>Hello World</h2>
                     <p className='text-black mb-6 text-lg font-semibold'>Welcome to Server</p>
