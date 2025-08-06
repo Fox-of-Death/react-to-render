2d21993194cf95eeb4db75f5e0b8b98b20007a36
Fox-of-Death <weerayut19082549@gmail.com>
2025-08-06 19:03:54 +0700

    first commit

--- src/services/courseData.js
+++ src/services/courseData.js
@@ -4,28 +4,28 @@ const courseData = [
         title: "Basic React Native",
         duration: 5,
         price: 7500,
-        imageUrl: "public/img/cat.jpg"
+        imageUrl: "/src/assets/img/cat.jpg"
     },
     {
         id: 2,
         title: "Power BI Dashboard",
         duration: 4,
         price: 5500,
-        imageUrl: "public/img/cat.jpg"
+        imageUrl: "/src/assets/img/cat.jpg"
     },
     {
         id: 3,
         title: "UI Design with Figma",
         duration: 3,
         price: 5000,
-        imageUrl: "public/img/cat.jpg"
+        imageUrl: "/src/assets/img/cat.jpg"
     },
     {
         id: 4,
         title: "Cross Platform with Flutter",
         duration: 5,
         price: 6500,
-        imageUrl: "public/img/cat.jpg"
+        imageUrl: "/src/assets/img/cat.jpg"
     },
 ];
 
